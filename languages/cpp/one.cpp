5389e4e56d82eb179bf53d7e67beeb3e72dd15ee
aandrews <aandreyev.aanton@gmail.com>
2017-05-23 23:58:29 +0300

    Marked x

---
+++ languages/cpp/one.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <sring>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+
+    const double PI = 3.14146;
+    char myGrade = 'A';
+}
